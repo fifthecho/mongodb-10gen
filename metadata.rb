55d5d67350e332d819c63ca7e4d02455b8eb74b8
Jeff Moody <fifthecho@gmail.com>
2013-02-06 11:24:06 -0500

    Remove depends

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,4 @@ license          "MIT"
 description      "Installs/Configures mongodb-10gen"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.8"
-depends          "apt"
-depends			 "yum"
 depends          "mongodb-10gen" # workaround for TravisCI
